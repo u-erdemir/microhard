5632e73d0c05e4165954ce611aeddaf495854245
hasannyr <37706376+hasannyr@users.noreply.github.com>
2021-06-01 11:19:18 +0300

    Update microhard-lib.py

--- microhard-lib.py
+++ microhard-lib.py
@@ -1,4 +1,5 @@
 from telnetlib import Telnet
+import time
 
 class Microhard():
     def __init__(self,host,username,password):
@@ -6,6 +7,7 @@ class Microhard():
         self.username = username
         self.password = password
         self.connect(self.host) 
+        self.get_status()
         
     def connect(self,host):
         self.tn = Telnet(host)
@@ -32,8 +34,8 @@ class Microhard():
         dec = data.decode('ascii').split('\r\n')
         self.frequency = int(dec[7].split(' ')[13])
         self.tx_power = int(dec[8].split(' ')[15])
-        self.rx_byte = int(dec[11].split(" ")[10].split("B")[0])
-        self.tx_byte = int(dec[13].split(" ")[9].split("B")[0])
+        self.rx_byte = dec[11].split(" ")[10].split("B")[0]
+        self.tx_byte = dec[13].split(" ")[9].split("B")[0]
         return dec
     
     def radio_on(self):
@@ -74,16 +76,45 @@ class Microhard():
         return dec
     
     def get_datarate(self):
-        asd = True
-        return asd
+        self.get_status()
+        i_1=self.rx_byte
+        time.sleep(3)
+        self.get_status()
+        i_2=self.rx_byte
+        if i_1[-1]=='T' and i_2[-1]=='T':
+            i_1=float(i_1[0:-1])*(2**40)
+            i_2=float(i_2[0:-1])*(2**40)  
+        elif i_1[-1]=='G' and i_2[-1]=='T':
+            i_1=float(i_1[0:-1])*(2**30)
+            i_2=float(i_2[0:-1])*(2**40)          
+        elif i_1[-1]=='G' and i_2[-1]=='G':
+            i_1=float(i_1[0:-1])*(2**30)
+            i_2=float(i_2[0:-1])*(2**30)
+        elif i_1[-1]=='M' and i_2[-1]=='G':
+            i_1=float(i_1[0:-1])*(2**20)
+            i_2=float(i_2[0:-1])*(2**30)
+        elif i_1[-1]=='M' and i_2[-1]=='M':
+            i_1=float(i_1[0:-1])*(2**20)
+            i_2=float(i_2[0:-1])*(2**20)
+        elif i_1[-1]=='K' and i_2[-1]=='M':
+            i_1=float(i_1[0:-1])*(2**10)
+            i_2=float(i_2[0:-1])*(2**20)
+        elif i_1[-1]=='K' and i_2[-1]=='K':
+            i_1=float(i_1[0:-1])*(2**10)
+            i_2=float(i_2[0:-1])*(2**10)
+        else:
+            print('Run get_datarate command again!')        
+        return str(round(((i_2-i_1)/3),2))+str("bps")
     
     def get_rssi(self):
         self.tn.write(b"AT+MWRSSI\n") 
         self.tn.read_until(b"OK")
         return True
     
-    def set_txpower(self,txpower):
-        self.tn.write(b"AT+MWTXPOWER=txpower\n")
+    def set_txpower(self,tx_power):
+        self.tx_power=tx_power
+        cmd="AT+MWTXPOWER="+str(tx_power)+"\n"
+        self.tn.write(cmd.encode('ascii'))
         self.tn.read_until(b"OK")
         return True
     
@@ -92,6 +123,8 @@ class Microhard():
     
 a = Microhard("192.168.168.3","admin","hisar123")
 data = a.get_status()
+# print(a.get_frequency())
+print(a.get_datarate())
+# print(a.get_snr())
+# print(a.get_status())
 
-print(a.get_snr())
-print(a.get_status())
